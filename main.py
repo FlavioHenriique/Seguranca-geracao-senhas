ef00197f8828c0e5ddeb44a9781326b97c6e88de
FlavioHenriique <flaviohenrique638@gmail.com>
2018-07-20 07:29:12 -0300

    Modificando algoritmo para printar apenas palavras de 9 ou 10 letras

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ texto = arquivo.read()
 
 palavras = texto.split()
 array = []
-
+print(len(palavras))
 for x in range(len(palavras)):
     palavra = "".join(e for e in palavras[x] if e.isalpha())
     if palavra != "":
         array.append(palavra)
 
 for k in range(len(array)):
-    print(array[k])
+    if len(array[k])==9 :
+            print(array[k])
