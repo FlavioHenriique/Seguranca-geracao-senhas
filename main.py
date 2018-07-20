a091af8e0cd7b0864317003d2aafd9e8c3a59ba1
FlavioHenriique <flaviohenrique638@gmail.com>
2018-07-20 07:34:45 -0300

    Corrigindo ultimo for

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ texto = arquivo.read()
 
 palavras = texto.split()
 array = []
-print(len(palavras))
+
 for x in range(len(palavras)):
     palavra = "".join(e for e in palavras[x] if e.isalpha())
     if palavra != "":
         array.append(palavra)
 
 for k in range(len(array)):
-    if len(array[k])==9 :
+    if len(array[k])==9 or len(array[k]) == 10:
             print(array[k])
